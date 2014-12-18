f86196fd47660fb4842ead6cc2456da49e659213
Nischay <nischay13144@iiitd.ac.in>
2014-12-19 01:00:22 +0530

    Add changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 3}
 	validates :content, presence: true
+	belongs_to :user
 end
